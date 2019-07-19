c5c8156d2d13cabfec048ad9b4448ec04a92741b
Ubuntu <ubuntu@ip-172-31-81-255.ec2.internal>
2019-07-19 16:37:24 +0000

    fix flash messages placement

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,3 @@
-/****************Flash***************/
-if($(".flash").css('display') === 'block'){
-	$(".navbar").css('box-shadow','none');
-	$(".flash").css('box-shadow','0 2em 1em -1em #222');
-}
-
-
 /****************Footer***************/
 checkFooterPosition();
 function checkFooterPosition(){
