93300d1ab685e8be808ad15e0e02a440bcc022f3
Ubuntu <ubuntu@ip-172-31-81-255.ec2.internal>
2019-07-30 20:48:50 +0000

    add google signin

--- public/scripts/googleSignIn.js
+++ public/scripts/googleSignIn.js
@@ -1,14 +1,24 @@
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+startApp();
+var googleUser = {};
+function startApp() {
+    gapi.load('auth2', function(){
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        auth2 = gapi.auth2.init({
+            client_id: '182323935673-gboaegp9j8rocvumaeqh43i8hero683m.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+            // Request scopes in addition to 'profile' and 'email'
+            //scope: 'additional_scope'
+        });
+        attachSignin(document.getElementById('googleBtn'));
+    });
+};
+
+function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {}, function(googleUser) {
+        document.getElementById('googleName').innerText = "Signed in: " +
+        googleUser.getBasicProfile().getName();
+    }, function(error) {
+        console.log(JSON.stringify(error, undefined, 2));
+    });
 }
