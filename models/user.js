1e6568aa05af90c424557044a01a2f9aaaa6cce8
Ubuntu <ubuntu@ip-172-31-81-255.ec2.internal>
2019-07-30 20:56:02 +0000

    update

--- models/user.js
+++ models/user.js
@@ -8,7 +8,10 @@ const userSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
 	email: String,
-	isAdmin: {type: Boolean, default: false}
+	isAdmin: {
+		type: Boolean,
+		default: false
+	}
 });
 
 userSchema.plugin(passportLocalMongoose);
